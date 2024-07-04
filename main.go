24b8fe3b5edc6f8b33727438a5c6c153b4910393
ahmedtofaha10 <ahmedtofaha999@gmail.com>
2024-07-04 23:33:24 +0300

    start cooking

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		EnableBashCompletion: true,
+		Usage:                "make documentations for your projects ;)",
+		Commands: []*cli.Command{
+			{
+				Name:    "laravel",
+				Aliases: []string{"L"},
+				Usage:   "start document of laravel project",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "path",
+						Usage:    "path for the base/root directory of the project",
+						Required: true,
+					},
+				},
+				Action: func(cCtx *cli.Context) error {
+					path := cCtx.String("path")
+					fmt.Println("start documenting:", path)
+					return nil
+				},
+				BashComplete: func(cCtx *cli.Context) {
+					// This will complete if no args are passed
+					if cCtx.NArg() > 0 {
+						return
+					}
+					fmt.Println("TASK NAME")
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
